d922cc665402a49d9c2d9712062b21cc5a27cfc6
Anushabhagwath <Anushabhagwath85@gmail.com>
2024-11-29 21:31:13 +0530

    skip spaces and detect digits

--- calculator.c
+++ calculator.c
@@ -3,6 +3,33 @@
 #include<ctype.h>
 #include<string.h>
 
+
+int evaluate(const char *expression, int *error) {
+    char operators[100];
+    int values[100];
+    int opTop = -1, valTop = -1;
+
+    int i = 0, len = strlen(expression);
+
+    while (i < len) {
+        if (isspace(expression[i])) {
+            i++;
+            continue;
+        }
+
+        if (isdigit(expression[i])) {
+            int value = 0;
+            while (i < len && isdigit(expression[i])) {
+                value = value * 10 + (expression[i] - '0');
+                i++;
+            }
+            values[++valTop] = value;
+        }
+    }
+        i++;
+    return 0; 
+}
+
 int main() {
     char expression[1000];
     printf("Enter the expression: ");
@@ -11,7 +38,7 @@ int main() {
     expression[strcspn(expression, "\n")] = 0;
 
     int error = 0;
-    int ans = evaluateExpression(expression, &error);
+    int ans = evaluate(expression, &error);
 
     if (error == 1) {
         printf("Error: Invalid expression.\n");
